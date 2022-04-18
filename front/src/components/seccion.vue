7a512fca11231b8ed6eec954e4387414719542ea
EduDAW <eal0003@alu.medac.es>
2022-04-18 17:57:16 +0200

    Mejoras de seccion

--- front/src/components/seccion.vue
+++ front/src/components/seccion.vue
@@ -3,26 +3,33 @@ export default {
   name:"seccion_general",
   data(){
     return{
-      tamMax: 5,
+      tamMax: 6,
       imagens: ['https://source.unsplash.com/random/150x200?sig=1',
                'https://source.unsplash.com/random/150x200?sig=2',
                'https://source.unsplash.com/random/150x200?sig=3',
                'https://source.unsplash.com/random/150x200?sig=4',
-               'https://source.unsplash.com/random/150x200?sig=5']
+               'https://source.unsplash.com/random/150x200?sig=5',
+               'https://source.unsplash.com/random/150x200?sig=6',
+               'https://source.unsplash.com/random/150x200?sig=7']
     }
   }
 }
 </script>
 
 <template>
-  
+
   <div class="container">    
   <h3>Los panas</h3><br>
   
   <div class="row" v-if="imagens.length>tamMax">
-    <div class="col-sm-2 " v-for="i in (tamMax)" :key="i">
+    <div class="col-sm-2 d-sm-3" v-for="i in (3)" :key="i">
       <a href="">
         <img :src="imagens[i-1]">
+      </a>
+    </div>
+    <div class="col-sm-2 d-none d-lg-block" v-for="i in (tamMax-3)" :key="i">
+      <a href="">
+        <img :src="imagens[i+2]">
       </a>
     </div>
   </div>
@@ -34,9 +41,14 @@ export default {
   </div>
 
   <div class="row" v-else >
+    <div class="col-sm-2">
+      <a href="">
+        <img :src="imagens[0]">
+      </a>
+    </div>
     <div class="col-sm-2" v-for="i in (imagens.length||tamMax)" :key="i">
       <a href="">
-        <img :src="imagens[i-1]">
+        <img :src="imagens[i]">
       </a>
     </div>
   </div>
