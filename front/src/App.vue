67038047f135694e1b3c764fbed4658e2c125018
LecheDeAvena <eal0003@alu.medac.es>
2022-04-18 15:23:49 +0200

    creacion de las secciones e implementacion en main

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,14 +1,24 @@
 <template>
   <navbar_first/>
+
+  <seccion :title="'Reseñas de amigos'" :imagens="['https://source.unsplash.com/random/150x200?sig=1',
+               'https://source.unsplash.com/random/150x200?sig=2',
+               'https://source.unsplash.com/random/150x200?sig=3',
+               'https://source.unsplash.com/random/150x200?sig=4',
+               'https://source.unsplash.com/random/150x200?sig=5']"/>
+
+  <seccion :title="'Sexy UwU'" :imagens="[]"/>
 </template>
 
 <script>
 import navbar_first from './components/navbar.vue'
+import seccion from './components/seccion.vue'
 
 export default {
   name: 'App',
   components: {
-    navbar_first
+    navbar_first,
+    seccion
   }
 }
 </script>
---
+++ front/src/components/seccion.vue
@@ -0,0 +1,50 @@
+<script>
+export default {
+  name:"seccion_general",
+  props:{
+    title: String,
+    imagens: Array
+  },
+  data(){
+    return{
+      tamMax: 5
+    }
+  }
+}
+</script>
+
+<template>
+  
+  <div class="container">    
+  <h3>{{ title || 'Placeholder' }}</h3><br>
+  
+  <div class="row" v-if="imagens.length>tamMax">
+    <div class="col-sm-2 " v-for="i in (tamMax)" :key="i">
+      <a href="">
+        <img :src="imagens[i-1]">
+      </a>
+    </div>
+  </div>
+
+  <div class="row" v-else-if="imagens.length===0">
+    <div class="col-sm-2">
+      <p>Nada :(</p>
+    </div>
+  </div>
+
+  <div class="row" v-else >
+    <div class="col-sm-2" v-for="i in (imagens.length||tamMax)" :key="i">
+      <a href="">
+        <img :src="imagens[i-1]">
+      </a>
+    </div>
+  </div>
+</div><br>
+  
+</template>
+
+<style>
+  .container {
+    margin: 2rem;
+  }
+</style>
