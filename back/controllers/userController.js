b5684ae6fcff3d1b44e091ab76a9bc0c963f4bcc
A10ciaFeka <jag0003@alu.medac.es>
2022-04-18 17:55:23 +0200

    Bug(userController.js) -- Validacion de usuario no funciona

--- back/controllers/userController.js
+++ back/controllers/userController.js
@@ -1,6 +1,6 @@
 const userController = {};
 const passwordValidator = require('../validator/passwordValidator');
-
+const userValidator = require('../validator/userValidator');
 
 /** --- GET REQUESTS --- */
 /**
@@ -53,6 +53,33 @@ userController.getUserById = (req,res) => {
     });
 }
 
+/**
+ * Get user by it's username
+ * 
+ * @param {*} req 
+ * @param {*} res 
+ */
+userController.getUserByNickname = (req,res) => {
+    const nickname = req.params.nickname;
+    const sql = "SELECT * FROM users WHERE nickname="+"'"+nickname+"'";
+
+    req.getConnection((err,conn)=>{
+        try{
+            conn.query(sql,(err,user)=>{
+                if(err) {
+                    res.json(err);
+                }else{
+                    res.json(user);
+                }
+            });
+            
+        }catch(err){
+            res.json({'error': "Couldn't connect to db"});
+        }
+    });
+}
+
+
 
 /** --- POST USER --- */
 /**
@@ -61,21 +88,26 @@ userController.getUserById = (req,res) => {
  * @param {*} req 
  * @param {*} res 
  */
-userController.createUser = (req, res) => {
+userController.createUser = async(req, res) => {
 
     const {user_nickname,user_password,user_email} = req.body;
     const hashedPwd = passwordValidator.setPassword(user_password);
-    const sql = `INSERT INTO users VALUES('','${user_nickname}','${hashedPwd}','${user_email}', '0')`;
-
+    const sql = `INSERT INTO users VALUES('','${user_nickname}','${hashedPwd}','${user_email}', '0', '')`;
+    
     req.getConnection((err,conn) => {
         try{
-            conn.query(sql, (err,user)=>{
-                if(err) {
-                    res.json(err);
-                } else {
-                    res.json({'id_user': user.insertId});
-                }
-            })
+            let resNick = userValidator.validateNickname(conn,user_nickname);
+            if(resNick){
+                conn.query(sql, (err,user)=>{
+                    if(err) {
+                        res.json(err);
+                    } else {
+                        res.json({'id_user': user.insertId});
+                    }
+                })
+            }else{
+                res.json({'error': "Nickname already exits"});
+            }
         }catch(err) {
             res.json({'error': "Couldn't connect to db"})
         }
