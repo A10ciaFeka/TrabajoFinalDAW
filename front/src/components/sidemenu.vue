eb9aafff0ce163fb695870f0df776c206e9e96da
TheTroyaFake <att0004@alu.medac.es>
2022-04-19 14:45:32 +0200

    wip(Sidemenu.vue) -- Añadido de variables, puntuaje dinamico y tema oscuro

--- front/src/components/sidemenu.vue
+++ front/src/components/sidemenu.vue
@@ -1,44 +1,5 @@
-
 <style>
-:root {
-            --color-inactivo: #5f5050;
-            --color-hover: #ffa400;
-        }
-        .valoracion {
-            display: flex;
-            flex-direction: row-reverse;
-        }
-        .valoracion button {
-            background-color: initial;
-            border: 0;
-            color: var(--color-inactivo);
-            transition: 1s all;
-        }
-        .valoracion button:hover {
-            cursor: pointer;
-            color: var(--color-hover);
-            transform: rotate(360deg);
-        }
-        button:nth-child(1):hover ~ button {
-            color: var(--color-hover);
-            transform: rotate(360deg);
-        }
-        button:nth-child(2):hover ~ button {
-            color: var(--color-hover);
-            transform: rotate(360deg);
-        }
-        button:nth-child(3):hover ~ button {
-            color: var(--color-hover);
-            transform: rotate(360deg);
-        }
-        button:nth-child(4):hover ~ button {
-            color: var(--color-hover);
-            transform: rotate(360deg);
-        }
-        button:nth-child(5):hover ~ button {
-            color: var(--color-hover);
-            transform: rotate(360deg);
-        }
+
 
 </style>
 <template>
@@ -49,142 +10,55 @@
 >
       <div class="container">
         <div class="row">
-          <div class="col-lg-2">
-            <div class="card">
-              <img
-                src="https://source.unsplash.com/random/150x200?sig=2"
-                class="card-img-top"
-                alt="Waterfall"
-              />
+          <div class="col-lg-2" v-for="game in games" :key="game">
+            <div class="card text-white bg-dark">
+              <img v-bind:src="game.Url" class="card-img-top" alt="" width="200" height="255"/>
               <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <div class="valoracion">
-                  <!-- Estrella 1 -->
-                  <button>
-                      <i class="fas fa-star"></i>
-                  </button>
-                  <!-- Estrella 2 -->
-                  <button>
-                      <i class="fas fa-star"></i>
-                  </button>
-                  <!-- Estrella 3 -->
-                  <button>
-                      <i class="fas fa-star"></i>
-                  </button>
-                  <!-- Estrella 4 -->
-                  <button>
-                      <i class="fas fa-star"></i>
-                  </button>
-                  <!-- Estrella 5 -->
-                  <button>
-                      <i class="fas fa-star"></i>
-                  </button>
-              </div>
+                <h5 class="card-title">{{game.name}}</h5>
+                 <star-rating :rating=game.rating :show-rating="false" :star-size="20" :read-only="true" :increment="0.01" :star-points="[23,2, 14,17, 0,19, 10,34, 7,50, 23,43, 38,50, 36,34, 46,19, 31,17]"></star-rating>
               </div>
             </div>
           </div>
 
-          <div class="col-lg-2">
-            <div class="card">
-              <img
-                src="https://source.unsplash.com/random/150x200?sig=3"
-                class="card-img-top"
-                alt="Sunset Over the Sea"
-              />
-              <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <div class="valoracion">
-                  <!-- Estrella 1 -->
-                  <button>
-                      <i class="fas fa-star"></i>
-                  </button>
-                  <!-- Estrella 2 -->
-                  <button>
-                      <i class="fas fa-star"></i>
-                  </button>
-                  <!-- Estrella 3 -->
-                  <button>
-                      <i class="fas fa-star"></i>
-                  </button>
-                  <!-- Estrella 4 -->
-                  <button>
-                      <i class="fas fa-star"></i>
-                  </button>
-                  <!-- Estrella 5 -->
-                  <button>
-                      <i class="fas fa-star"></i>
-                  </button>
-              </div>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-lg-2">
-            <div class="card">
-              <img
-                src="https://source.unsplash.com/random/150x200?sig=1"
-                class="card-img-top"
-              />
-              <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <div class="valoracion">
-                  <!-- Estrella 1 -->
-                  <button>
-                      <i class="fas fa-star"></i>
-                  </button>
-                  <!-- Estrella 2 -->
-                  <button>
-                      <i class="fas fa-star"></i>
-                  </button>
-                  <!-- Estrella 3 -->
-                  <button>
-                      <i class="fas fa-star"></i>
-                  </button>
-                  <!-- Estrella 4 -->
-                  <button>
-                      <i class="fas fa-star"></i>
-                  </button>
-                  <!-- Estrella 5 -->
-                  <button>
-                      <i class="fas fa-star"></i>
-                  </button>
-              </div>
-              </div>
-            </div>
-          </div>
         </div>
       </div>
 </div>
 </template>
 
 <script>
-
+import StarRating from 'vue-star-rating'
 export default {
   name: 'lateral_menu',
+  components: {
+  StarRating
+  },
   props: {
     msg: String
   },
   data(){
     return{
-      
+       img: '../assets/img/eldenring.png',
+      games: [
+        {
+          name: 'Elden Ring',
+          rating: 4,
+          Url: 'https://uvejuegos.com/img/caratulas/62849/Elden-Ring-portada-uvejuegos.png'
+        },
+        {
+          name: 'Peppa pig',
+          rating: 5,
+          Url: 'https://fr.web.img3.acsta.net/pictures/20/03/30/12/25/1591799.jpg'
+        },
+        {
+          name: 'Pierde vidas',
+          rating: 0.5,
+          Url: 'https://as01.epimg.net/meristation/imagenes/2019/08/07/cover/719414081565191040.jpg'
+        }
+      ]
     }
-  },
-  mounted(){
-  const ratings = {
-    hotel_a : 2.8,
-    hotel_b : 3.3,
-    hotel_c : 1.9,
-    hotel_d : 4.3,
-    hotel_e : 4.74
-  };
-    const starTotal = 5;
-
-  for(const rating in ratings) {  
-    const starPercentage = (ratings[rating] / starTotal) * 100;
-    const starPercentageRounded = `${(Math.round(starPercentage / 10) * 10)}%`;
-    document.querySelector(`.${rating} .stars-inner`).style.width = starPercentageRounded; 
   }
-    }
+  
+ 
   }
 
 </script>
