0038475a9db0abbb4c89532020d606c31f7579c3
LecheDeAvena <eal0003@alu.medac.es>
2022-04-19 02:22:22 +0200

    seccion -> arreglo responsive

--- front/src/components/seccion.vue
+++ front/src/components/seccion.vue
@@ -22,33 +22,48 @@ export default {
   <h3>Los panas</h3><br>
   
   <div class="row" v-if="imagens.length>tamMax">
-    <div class="col-sm-2 d-sm-3" v-for="i in (3)" :key="i">
+    <div class="col-lg-2 col-md-3 col-sm-4 col-xs-1 col-auto" v-for="i in (2)" :key="i">
       <a href="">
         <img :src="imagens[i-1]">
       </a>
     </div>
-    <div class="col-sm-2 d-none d-lg-block" v-for="i in (tamMax-3)" :key="i">
+    <div class="col-lg-2 col-md-3 d-none d-lg-block d-md-block" v-for="i in (tamMax-4)" :key="i">
       <a href="">
         <img :src="imagens[i+2]">
+      </a>
+    </div>
+    <div class="col-lg-2 d-none d-lg-block" v-for="i in (tamMax-2)" :key="i">
+      <a href="">
+        <img :src="imagens[i+4]">
       </a>
     </div>
   </div>
 
   <div class="row" v-else-if="imagens.length===0">
-    <div class="col-sm-2">
+    <div class="col-lg-2">
       <p>Nada :(</p>
     </div>
   </div>
 
   <div class="row" v-else >
-    <div class="col-sm-2">
+    <div class="col-lg-2 col-md-3 col-sm-4 col-xs-1 col-auto">
       <a href="">
         <img :src="imagens[0]">
       </a>
     </div>
-    <div class="col-sm-2" v-for="i in (imagens.length||tamMax)" :key="i">
+    <div class="col-lg-2 col-md-3 col-sm-4 col-xs-1 col-auto" v-if="imagens.length>=2">
       <a href="">
-        <img :src="imagens[i]">
+        <img :src="imagens[1]">
+      </a>
+    </div>
+    <div class="col-lg-2 col-md-3 d-none d-lg-block d-md-block" v-if="imagens.length>=3">
+      <a href="">
+        <img :src="imagens[2]">
+      </a>
+    </div>
+    <div class="col-lg-2 d-none d-lg-block" v-for="i in (imagens.length-3)" :key="i">
+      <a href="">
+        <img :src="imagens[i+2]">
       </a>
     </div>
   </div>
