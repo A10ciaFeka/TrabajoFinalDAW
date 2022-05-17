770f07d9c17e69ad57807752e684b51a839c1014
LecheDeAvena <eal0003@alu.medac.es>
2022-05-17 03:54:09 +0200

    wip(profile.vue) -- cambio en la estructura de la vista, falta sacar la informacion de los productos para colocarlos en las reviews recientes

--- front/src/views/profile.vue
+++ front/src/views/profile.vue
@@ -12,64 +12,56 @@
                         <div class="row">
                             <div class=" d-flex flex-column col-2 align-items-center">
                                 <img src="https://static.scientificamerican.com/espanol/cache/file/AF74ADFC-11A1-4BE8-8C6D050938F3CD40_source.jpg"
-                                    class="mb-2 rounded"
+                                    class="mb-2 rounded-circle"
                                     width="130" height="130">
                             </div>
-                
-                            <div class="col-9">
-                                <h2 class="fw-bold">{{usuario.usuario_apodo}}</h2>
-                                
-                                <div class="">
-                                    <span class="">{{this.reviews.length}} Reviews</span>
-                                    <span class="ms-3">{{this.amigos.length}} Amigos</span>
-                                </div>
 
-                                <button type="button" class="mt-2 bg-primary">
-                                    Editar perfil
+                            <div class="col-7 mt-2">
+                                <h2 class="fw-bold">{{usuario.usuario_apodo}}</h2>
+
+                                <button type="button" class="btn btn-secondary bg-secondary rounded btn-sm">
+                                    EDITAR PERFIL
                                 </button>
+                            </div>
+
+                            <div class="col-3 mt-4">
+                                <span class="h5">{{this.reviews.length}} Reviews</span>
+                                <span class="h5 ms-3">{{this.amigos.length}} Amigos</span>
                             </div>
                         </div>
 
-                        <div class="ms-4 mt-5">
+                        <div class="ms-4 mt-5 me-5">
                             
-                            <h5>Mis últimas reviews</h5>
-                                
-                                <div class="mt-4 border-bottom" v-for="review in reviews" :key="review">
-                                    <div class="mt-3 mb-4 d-flex flex-row comentario">
-                                        <div class="col-2">
-                                            <div class="mb-2 ms-3">
-                                            <img :src="`usuarios/${review.usuario_fotoPerfil}`" height="50" width="50">
-                                            </div>
-                                            <p class="mb-1 bg-success col-6 text-center rounded">
-                                            <strong>{{review.usuario_apodo}}</strong>
-                                            </p>
+                            <div class="d-flex justify-content-between">
+                              <h5>Reviews recientes</h5>
+                              <span>Mostrar más</span> 
+                            </div>
+                            
+                            <div class="border-top" v-for="review in reviews" :key="review">
+                                <div class="mt-3 mb-4 d-flex flex-row">
+                                    <div class="col-2">
+                                        <div class="mb-2">
+                                        <img :src="`usuarios/${review.usuario_fotoPerfil}`" height="160" width="120">
                                         </div>
-
-                                        <div class="col-10">
-                                        
-                                            <p class="mt-2 d-flex">
-                                            <span class="me-2"><u class=" fs-5">{{review.review_nombre}}</u></span>
-
-                                            <star-rating :rating="review.review_estrellas" :show-rating="false" :star-size="20" :read-only="true" :increment="0.01" :star-points="[23,2, 14,17, 0,19, 10,34, 7,50, 23,43, 38,50, 36,34, 46,19, 31,17]"></star-rating>
-                                            </p>
-                                            <p class="text-lighter" v-if="review.review_texto!='null'">
-                                            {{review.review_texto}}
-                                            </p>
-                                        </div>
+                                        <p class="mb-1 bg-success col-6 text-center rounded">
+                                        <strong>{{review.usuario_apodo}}</strong>
+                                        </p>
                                     </div>
-                                </div>
-                            <h5 class="mt-4">Mis amigos</h5>
-                            <div class="row justify-content-start">
-                                <div class="col-md-4 col-lg-2 col-6" v-for="amigo in amigos" :key="amigo">
-                                    <div class="mt-3 mb-4">
-                                        <div class="">
-                                            <div class="mb-2 ms-3 w-20">
-                                            <img :src="`usuarios/${amigo.usuario_fotoPerfil}`" height="50" width="50">
-                                            </div>
-                                            <p class="mb-1 bg-success col-6 text-center rounded">
-                                            <strong>{{amigo.usuario_apodo}}</strong>
-                                            </p>
+
+                                    <div class="col-10">
+                                    
+                                        <span class="">
+                                          <strong class="h4 fw-bold">{{review.review_nombre}}</strong>
+                                        </span>
+                                        
+                                        <div class="d-flex">
+                                          <star-rating :rating="review.review_estrellas" :show-rating="false" :star-size="15" :read-only="true" :increment="0.01" :star-points="[23,2, 14,17, 0,19, 10,34, 7,50, 23,43, 38,50, 36,34, 46,19, 31,17]"></star-rating>
+                                          <span class="mt-1 ms-2 small text-muted">{{date(review.review_fecha)}}</span>
                                         </div>
+
+                                        <p class="mt-3 text-lighter" v-if="review.review_texto!='null'">
+                                        {{review.review_texto}}
+                                        </p>
                                     </div>
                                 </div>
                             </div>
@@ -88,6 +80,8 @@
 import navbar from '@/components/navbar.vue';
 import foter from '@/components/footer.vue';
 import axios from 'axios';
+import StarRating from 'vue-star-rating';
+import moment from 'moment';
 
 
 export default {
@@ -103,7 +97,8 @@ export default {
   },
   components: {
     foter,
-    navbar
+    navbar,
+    StarRating
   },
   mounted(){
     this.crearPagina();
@@ -133,6 +128,11 @@ export default {
     },
     crearPagina(){
       this.id_usuario= this.$route.query.id_usuario;
+    },
+    date(fecha){
+      let res=fecha;
+      res= moment(res).format('DD-MM-YYYY');
+      return res;
     }
   }
     
