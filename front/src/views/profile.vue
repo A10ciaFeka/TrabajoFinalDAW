c5ead0a9263f5ccde218a83742d6bd8b3dddcc03
A10ciaFeka <jag0003@alu.medac.es>
2022-05-19 22:36:35 +0200

    wip

--- front/src/views/profile.vue
+++ front/src/views/profile.vue
@@ -19,6 +19,10 @@
 .card{
     max-width: 230px !important;
 }
+.comment{
+    width:100px;
+    height:150px;
+}
 
 @media (max-width: 576px){
     .card-img-top{
@@ -39,22 +43,39 @@
     padding-right: 1em;
 }
 
-<<<<<<< HEAD
-=======
 .imgBorder{
   border: 0.1px solid #6E7C7C;
 }
-
-
+.bordeabajo{
+    border-bottom: 1px solid gray;
+}
+.nombre:hover{
+    color: #40BCF4 !important;
+}
 .perfil{
   border: 2px solid #6E7C7C;
+  box-shadow: 5px 6px 6px 1px #151515;
+}
+.perfil2{
+  border: 2px solid #6E7C7C;
+}
+.perfil2:hover{
   box-shadow: 5px 6px 6px 1px #151515;
 }
 
 .products:hover{
   box-shadow: 9px 10px 10px 5px #151515;
 }
->>>>>>> 34d9b32371067ef006316dd88ed97f898167d17b
+.cards{
+  max-width: 255px !important;
+  transition: all 0.2s ease;
+  cursor: pointer;
+}
+.cards:hover{
+    z-index: 3;
+  box-shadow: 9px 10px 10px 5px #151515;
+  transform: scale(1.1);
+}
 
 </style>
 <template>
@@ -64,17 +85,10 @@
       <div class="pt-5 ps-3 d-flex justify-content-between">
         <div class="d-flex ">
             <div>
-<<<<<<< HEAD
-                <img v-bind:src="'usuarios/' + imagen" class="rounded-circle" height="150" width="150" background-size="100% auto" background-position="50%" alt="">
-            </div>
-            <div>
-                <h2 class="mt-4 ms-5">{{usuario.usuario_apodo}}</h2>
-=======
                 <img v-bind:src="'usuarios/' + imagen" class="rounded-circle perfil" height="150" width="150" background-size="100% auto" background-position="50%" alt="">
             </div>
             <div>
                 <h2 class="mt-4 ms-3 fuente">{{usuario.usuario_apodo}}</h2>
->>>>>>> 34d9b32371067ef006316dd88ed97f898167d17b
             </div>
         </div>
         <div class="d-flex mt-4" style="height: 70px;">
@@ -105,63 +119,114 @@
         </div>
       </div>
 
-      <!-- Productos -->
-    <div class="d-flex mt-3">
-<<<<<<< HEAD
-        <div class="overflow-auto text-center">
-=======
-        <div class="text-center">
->>>>>>> 34d9b32371067ef006316dd88ed97f898167d17b
-            <h3>Juegos valorados</h3>
-            <hr>
-        <div class="container pt-3">
-            <div class="row">
-<<<<<<< HEAD
-                <div class="col-3" v-for="producto in productos" :key="producto">
-                    <div class="card bg-dark box bar">
-                        <router-link :to="{path:'/producto',query:{id_producto:producto.id_producto}}">
-                        <img v-bind:src="`productos/${producto.producto_imagen}`" class="card-img-top" alt="" width="185" height="255"/>
-=======
-                <div class="col-3 " v-for="producto in productos" :key="producto">
-                    <div class="card bg-dark box bar products">
-                        <router-link :to="{path:'/producto',query:{id_producto:producto.id_producto}}">
-                            <img v-bind:src="`productos/${producto.producto_imagen}`" class="card-img-top imgBorder" alt="" width="185" height="255"/>
->>>>>>> 34d9b32371067ef006316dd88ed97f898167d17b
-                        </router-link>
+        <div class="d-flex">
+            <div class="col-9">
+                <!-- Productos -->
+                <div class="d-flex mt-5">
+                    <div class="overflow-hiden">
+
+                        <div class="mt-3">
+
+                            <h4 class="fuente ms-4">JUEGOS VALORADOS</h4>
+                            <hr class="bg-success mx-2 me-5">
+                        </div>
+                        <div class="container pt-1">
+                            <div class="row">
+
+                                <div class="col-1 " v-for="producto in productos" :key="producto">
+                                    <div class="card bg-dark cards box bar products">
+                                        <router-link :to="{path:'/producto',query:{id_producto:producto.id_producto}}">
+                                            <img v-bind:src="`productos/${producto.producto_imagen}`" class="card-img-top imgBorder" alt="" width="185" height="255"/>
+                                        </router-link>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </div>
-        </div>
-    </div>
 
-        <!-- Seguidos -->
-        <div class="text center mt-3 w-100">
-            <h3 class="text-center">Seguidos</h3>
-            <hr>
-        </div>
-        <div class="d-flex">
-            <div class="overflow-auto text-center">
-            <div class="container pt-3">
-                <div class="row">
-                    <div class="col-3 ms-2"  v-for="idamigo in idamigos" :key="idamigo">
-<<<<<<< HEAD
-                        <div class="mx-2  d-flex flex-column">
-=======
-                        <div class="mx-5 d-flex flex-column">
->>>>>>> 34d9b32371067ef006316dd88ed97f898167d17b
-                            <div>
-                                <img v-bind:src="`usuarios/${idamigo.usuario_fotoPerfil}`" class="rounded-circle" height="100" width="100" background-size="100% auto" background-position="50%" alt="">
+                <!-- reviews -->
+                <div class="mt-5 w-100">
+                    <h4 class="fuente ms-4">REVIEWS RECIENTES</h4>
+                    <hr class="bg-success mx-2 me-5">
+                </div>
+                <div class="d-flex w-100 mb-3">
+
+                    <div class="pt-3 w-100">
+                        <div class="d-flex w-100 flex-column">
+                            <div class="w-100 ms-2 mt-3"  v-for="review in reviews" :key="review">
+                                <div class="d-flex w-100" v-for="producto in productos" :key="producto">
+                                    <div v-if="producto.id_producto == review.id_producto" class="d-flex bordeabajo w-100 pb-3">
+                                        <div class="w-30">
+                                            <img v-bind:src="`productos/${producto.producto_imagen}`" class="comment imgBorder" alt="" width="185" height="255"/>
+                                        </div>
+                                        <div class="d-flex w-70 flex-column ms-5">
+                                            <div>
+                                                <h5 class="nombre fuente">{{producto.producto_nombre}}</h5>
+                                            </div>
+                                            <div class="d-flex">
+                                                <div>
+                                                    <!-- <span>{{review.review_nombre}}</span> -->
+                                                    <star-rating :rating="review.review_estrellas" :show-rating="false" :star-size="13" :read-only="true" :increment="0.01" :star-points="[23,2, 14,17, 0,19, 10,34, 7,50, 23,43, 38,50, 36,34, 46,19, 31,17]"></star-rating>
+                                                </div>
+                                                <div class="mx-2">
+                                                    <span class="text-muted">{{review.review_fecha}}</span>
+                                                </div>
+                                            </div>
+                                            <div>
+                                                <span>{{review.review_texto}}</span>
+                                            </div>
+                                        </div>
+                                    </div>     
+                                </div>
                             </div>
-                            <div class="ms-3">
-                                <span>{{idamigo.usuario_apodo}}</span>
+                        </div>
+                    </div>
+                </div>
+                <!-- <div class="d-flex mb-4">
+                    <div class="overflow-auto text-center">
+                        <div class="container pt-3">
+                            <div class="row">
+                                <div class="col-3 ms-2"  v-for="idamigo in idamigos" :key="idamigo">
+
+                                    <div class="mx-5 d-flex flex-column">
+                                        <div>
+                                            <img v-bind:src="`usuarios/${idamigo.usuario_fotoPerfil}`" class="rounded-circle perfil2" height="100" width="100" background-size="100% auto" background-position="50%" alt="">
+                                        </div>
+                                        <div class="ms-3">
+                                            <span>{{idamigo.usuario_apodo}}</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div> -->
+            </div>
+            <div class="col-3 mt-3">
+                <div class="text center mt-5 w-100">
+                    <h4 class="fuente text-center">SEGUIDOS</h4>
+                    <hr class="bg-success mx-4">
+                </div>
+                <div class="container pt-3">
+                    <div class="row">
+                        <div class="col-4 ms-2 text-center mb-3 me-2"  v-for="idamigo in idamigos" :key="idamigo">
+
+                            <div class="mx-5 d-flex w-100 flex-column text-center">
+                                <div>
+                                    <img v-bind:src="`usuarios/${idamigo.usuario_fotoPerfil}`" class="rounded-circle perfil2" height="100" width="100" background-size="100% auto" background-position="50%" alt="">
+                                </div>
+                                <div class="text-center">
+                                    <span>{{idamigo.usuario_apodo}}</span>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-            </div>
         </div>
+        
+
         
     </div>
 
@@ -174,6 +239,8 @@
 import navbar from '@/components/navbar.vue';
 import foter from '@/components/footer.vue';
 import axios from 'axios';
+import moment from 'moment';
+import StarRating from 'vue-star-rating'
 
 export default {
   name: 'profile_page',
@@ -183,7 +250,7 @@ export default {
   components: {
     foter,
     navbar,
-    //StarRating
+    StarRating
   },
   data() {
       return{
@@ -191,6 +258,7 @@ export default {
         imagen: null,
         productos: [],
         idamigos:[],
+        reviews:[]
       }
   },
   mounted(){
@@ -202,9 +270,10 @@ export default {
       this.imagen = this.usuario.usuario_fotoPerfil
     }
     console.log(this.usuario);
-    axios.get(`http://localhost:3000/producto/listar/4/0`)
+    axios.get(`http://localhost:3000/producto/listar/7/0`)
     .then((response)=>{
           this.productos = response.data.resultados
+          
           console.log(this.productos);
     });
     axios.get(`http://localhost:3000/usuario/${this.usuario.id_usuario}/amigos`)
@@ -212,10 +281,18 @@ export default {
         this.idamigos = response.data
         console.log(this.idamigos);
     });
+        axios.get(`http://localhost:3000/review/usuario/${this.usuario.id_usuario}`)
+    .then((response)=>{
+        this.reviews = response.data
+        for (const review in this.reviews) {
+              this.reviews[review].review_fecha = moment(this.reviews[review].review_fecha).format('DD-MM-YYYY');
+              }
+        console.log(this.reviews);
+    });
     
                 // let json ={
                 //     "id_usuario" : '7',
-                //     "id_amigo" : '5',
+                //     "id_amigo" : '4',
 
                 // };
                 // axios.post('http://localhost:3000/usuario/seguir_usuario',json)
@@ -230,6 +307,18 @@ export default {
                 //     }
                 // })
             
+  },
+  methods:{
+    //   obtenerproduct(){
+    //       for (const review in this.reviews) {
+    //           for (const producto in this.productos) {
+    //               if(this.reviews[review].id_producto == this.productos ){
+
+    //         }
+    //           }
+            
+    //       }
+    //   }
   },
   beforeCreate () {
     document.querySelector('body').classList.remove('fondo2');
